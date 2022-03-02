44955b423910d3adc9f893a7de769617a59432dc
Lexi Dorrity <lexidorrity@Lexis-MacBook-Pro.local>
2022-03-02 10:40:07 -0700

    added questions to javascript and added event listner for start button

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # code-quiz
-Git Hub: https://github.com/Lexi-Diamond/code-quiz.git
-Live URL:  https://lexi-diamond.github.io/code-quiz/
 
-Screenshot
+
 
 
---
+++ assets/script.js
@@ -0,0 +1,16 @@
+var questions = [
+    {
+        questionTitle: "What does DOM stand for?",
+        possibleAnswers: ["Dogs of the Metaverse", "Dominion of Math", "Document Object Model"],
+        correctAnswer: "Document Object Model"
+    },
+    {
+        questionTitle: "What does DOM stand for?",
+        possibleAnswers: ["Dogs of the Metaverse", "Dominion of Math", "Document Object Model"],
+        correctAnswer: "Document Object Model"  
+    }
+]
+var startBtn = document.querySelector("#startButton");
+startBtn.addEventListener("click", function (){
+    console.log("clicked")
+});
