69f65d3a307f0952a5790aac4fc0c8d31c509053
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-04-25 06:16:36 -0700

    started over

---
+++ assets/questions.js
@@ -0,0 +1,33 @@
+var questions = [
+    {
+        title: "What does DOM stand for?",
+        choices: ["Dogs of the Metaverse", "Dominion of Math", "Document Object Model"],
+        answer: "Document Object Model"
+    },
+    {
+        title: "What word does the C refer to in CSS?",
+        choices: ["Cats", "Cascading", "Cookies"],
+        answer: "Cascading"  
+    },
+    {
+        title: "What is Bootstrap used for?",
+        choices: ["Styling Framework", "Database Creation", "Code editing tool"],
+        answer: "Styling Framework"  
+    },
+    {
+        title: "Which is not a Javascript dataype?",
+        choices: ["Boolean", "String", "Carrot"],
+        answer: "Carrot"  
+    },
+    {
+        title: "What do Const and Var have in Common?",
+        choices: ["Nothing", "They are both scoped globally", "They are the exact same" ],
+        answer: "They are both scoped globally"  
+    },
+    {
+        title: "A key _____ pairs are used by objects to store data as properties?",
+        choices: ["Value", "Lock", "Scope" ],
+        answer: "Value"  
+    },
+
+]
